b38ee8dc6bdc49de682dd002a72e30aedbe52a5a
chenx23333@gmail.com <chenx23333@gmail.com>
2021-09-01 12:24:13 -0400

    fix and update HelloWorld.js

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,9 @@
-package helloworld;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.print("Hello World");
 	}
 
 }
+
